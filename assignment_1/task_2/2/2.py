b06e1b6a280a8a676151601177a1b0451774fdbc
Sharath Gowda <54208352+sharathgowda815@users.noreply.github.com>
2020-05-10 22:36:48 +0530

    Add files via upload

---
+++ assignment_1/task_2/2/2.py
@@ -0,0 +1,9 @@
+for i in range(0, 5):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+
+for i in range(5, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
